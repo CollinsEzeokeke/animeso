77e5cdca690b1f21ef1d6766f1882e04eebda612
Ezeokeke Collins Chidubem <90806850+CollinsEzeokeke@users.noreply.github.com>
2025-03-24 21:50:00 -0700

    Enhance EnhancedSmoothCarousel with improved card content and layout
    - Introduce a new TopReturn function to dynamically render card content based on card ID.
    - Update BgReturn function to enhance background styling and structure for each card.
    - Refactor ImageReturn function for better image handling and responsiveness.
    - Adjust layout styles for improved visual consistency across different screen sizes.

--- components/enhanced-smooth-carousel.tsx
+++ components/enhanced-smooth-carousel.tsx
@@ -4,7 +4,6 @@ import { useRef } from "react"; //useState, useEffect, useCallback
 import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-// import Image from "next/image";
 
 interface CardProps {
   id: string;
@@ -36,62 +35,6 @@ export default function EnhancedSmoothCarousel({
   useMotionValueEvent(scrollX, "change", (latest) => {
     console.log("scrollX", latest);
   });
-  const ImageReturn = ({ cardId }: { cardId: number }) => {
-    switch (cardId) {
-      case 0:
-        return (
-          <div className="w-[17.5vw] relaitve h-[40vh] bg-blue-500/45 z-0 flex items-center">
-            <Image
-              src="/feature-nlp-preview.webp"
-              width={400}
-              height={800}
-              alt=""
-            />
-          </div>
-        );
-      case 1:
-        return (
-          <div className="w-[17.5vw] relaitve h-[40vh] bg-blue-500/45 z-0 flex items-center">
-            <Image
-              src="/feature-widgets-preview.webp"
-              width={400}
-              height={800}
-              alt=""
-            />
-          </div>
-        );
-      case 2:
-        return (
-          <div className="w-[17.5vw] h-[40vh] bg-blue-500/45 z-0 flex items-center">
-            <Image
-              src="/feature-accounts-preview.webp"
-              width={400}
-              height={800}
-              alt=""
-            />
-          </div>
-        );
-      case 3:
-        return (
-          <div className="w-[17.5vw] relaitve h-[40vh] bg-blue-500/45 z-0 flex items-center">
-            <Image src="/avatar.png" width={400} height={800} alt="" />
-          </div>
-        );
-      case 4:
-        return (
-          <div className="w-[17.5vw] relaitve h-[40vh] bg-blue-500/45 z-0 flex items-center">
-            <Image
-              src="/feature-timezones-preview.webp"
-              width={400}
-              height={800}
-              alt=""
-            />
-          </div>
-        );
-      default:
-        return "";
-    }
-  };
 
   const TopReturn = ({
     cardId,
@@ -103,43 +46,198 @@ export default function EnhancedSmoothCarousel({
     switch (cardId) {
       case 0:
         return (
-          <div className="bg-black relative z-10">
-            <h3 className="text-xl font-semibold mb-2">{cardTitle}</h3>
-            <p>Card content placeholder</p>
+          <div className="relative z-10 h-[15%]">
+            <div className="flex items-center">
+              <Image src="/feature-nlp.webp" width={60} height={80} alt="" />
+              <h3 className="text-2xl font-bold mb-2">{cardTitle}</h3>
+            </div>
+            <div className="h-full font-bold text-[#BDBDBC] text-lg flex w-[80%] pl-3">
+              <p className="w-full h-full">
+                Schedule your todos using natural language, Amie understands
+                you.
+              </p>
+            </div>
           </div>
         );
       case 1:
         return (
-          <div className="bg-black relative z-10">
-            <h3 className="text-xl font-semibold mb-2">{cardTitle}</h3>
-            <p>Card content placeholder</p>
+          <div className="relative z-10 h-[15%]">
+            <div className="flex items-center">
+              <Image src="/feature-widgets.webp" width={60} height={80} alt="" />
+              <h3 className="text-2xl font-bold mb-2 text-black">{cardTitle}</h3>
+            </div>
+            <div className="h-full font-bold text-[#848383] text-lg flex w-[88%] pl-3">
+              <p className="w-full h-full">
+                Birthday, todos and schedules - only a glance away.
+              </p>
+            </div>
           </div>
         );
       case 2:
         return (
-          <div className="bg-black relative z-10">
-            <h3 className="text-xl font-semibold mb-2">{cardTitle}</h3>
-            <p>Card content placeholder</p>
+          
+          <div className="relative z-10 h-[15%]">
+            <div className="flex items-center">
+              <Image src="/feature-accounts.webp" width={60} height={80} alt="" />
+              <h3 className="text-xl font-semibold mb-2 text-black">{cardTitle}</h3>
+            </div>
+            <div className="h-full font-bold text-[#848383] text-lg flex w-[88%] pl-3">
+              <p className="w-full h-full">
+                All your accounts, happily together in one place.
+              </p>
+            </div>
           </div>
         );
       case 3:
         return (
-          <div className="bg-black relative z-10">
-            <h3 className="text-xl font-semibold mb-2">{cardTitle}</h3>
-            <p>Card content placeholder</p>
+          
+          <div className="relative z-10 h-[15%]">
+            <div className="flex items-center">
+              <Image src="/feature-pomodoro.webp" width={60} height={80} alt="" />
+              <h3 className="text-xl font-semibold mb-2 text-black">{cardTitle}</h3>
+            </div>
+            <div className="h-full font-bold text-[#848383] text-lg flex w-[90%] pl-3">
+              <p className="w-full h-full">
+                {/* All your accounts, happily together in one place. */}
+                Find the right balance between focus and breaks.
+              </p>
+            </div>
           </div>
         );
       case 4:
         return (
-          <div className="bg-black relative z-10">
-            <h3 className="text-xl font-semibold mb-2">{cardTitle}</h3>
-            <p>Card content placeholder</p>
+          
+          <div className="relative z-10 h-[15%]">
+            <div className="flex items-center space-x-2">
+              <Image src="/feature-timezones.webp" width={50} height={40} alt="" />
+              <h3 className="text-xl font-semibold mb-2 text-white/70">{cardTitle}</h3>
+            </div>
+            <div className="h-full font-bold text-[#848383] text-lg flex w-[88%] pl-3 pt-2">
+              <p className="w-full h-full">
+                Travelling? Amie helps you judge multiple timezones so you can beat the jet lag.
+              </p>
+            </div>
+          </div>
+          // <div className="bg-black relative z-10">
+          //   <h3 className="text-xl font-semibold mb-2">{cardTitle}</h3>
+          //   <p>Card content placeholder</p>
+          // </div>
+        );
+      default:
+        return "";
+    }
+  };
+
+  const ImageReturn = ({ cardId }: { cardId: number }) => {
+    switch (cardId) {
+      case 0:
+        return (
+          <div className="w-[17vw] relaitve h-[40vh] z-0 flex flex-col justify-end">
+            <Image
+              src="/feature-nlp-preview.webp"
+              width={400}
+              height={800}
+              className="w-auto h-auto scale-x-100 -translate-x-2"
+              alt=""
+            />
+            <div className="w-full h-[10%] flex justify-center items-center font-bold text-[#BDBDBC] text-base">
+              <p>Available on Pro</p>
+            </div>
+          </div>
+        );
+      case 1:
+        return (
+          
+          <div className="w-[17vw] relaitve h-[40vh] z-0 flex flex-col justify-center translate-y-4">
+            <Image
+              src="/feature-widgets-preview.webp"
+              width={400}
+              height={800}
+              className="w-auto h-auto scale-x-100 -translate-x-2"
+              alt=""
+            />
+          </div>
+        );
+      case 2:
+        return (
+          
+          <div className="w-[17vw] relaitve h-[40vh] z-0 flex flex-col justify-center translate-y-4">
+            <Image
+              src="/feature-accounts-preview.webp"
+              width={400}
+              height={800}
+              className="w-auto h-auto scale-x-100 -translate-x-2"
+              alt=""
+            />
+          </div>
+        );
+      case 3:
+        return (
+          <div className="w-[17vw] relaitve h-[40vh] bg-blue-500/45 z-0 flex items-center">
+            <Image src="/avatar.png" width={400} height={800} alt="" />
+          </div>
+        );
+      case 4:
+        return (
+          
+          <div className="w-[17vw] relaitve h-[40vh] z-0 flex flex-col justify-center translate-y-4">
+            <Image
+              src="/feature-timezones-preview.webp"
+              width={400}
+              height={800}
+              className="w-auto h-auto scale-x-100 -translate-x-2"
+              alt=""
+            />
           </div>
         );
       default:
         return "";
     }
   };
+
+  const BgReturn = ({
+    cardId,
+    children,
+  }: {
+    cardId: number;
+    children: React.ReactNode;
+  }) => {
+    switch (cardId) {
+      case 0:
+        return (
+          <div className="bg-gray-700/80 bg-card rounded-[2.5rem] p-6 h-[50.5vh] shadow-md">
+            {children}
+          </div>
+        );
+      case 1:
+        return (
+          <div className="bg-gray-300/80 bg-card rounded-[2.5rem] p-6 h-[50.5vh] shadow-md">
+            {children}
+          </div>
+        );
+      case 2:
+        return (
+          <div className="bg-gray-300/80 bg-card rounded-[2.5rem] p-6 h-[50.5vh] shadow-md">
+            {children}
+          </div>
+        );
+      case 3:
+        return (
+          <div className="bg-gray-300/80 bg-card rounded-[2.5rem] p-6 h-[50.5vh] shadow-md">
+            {children}
+          </div>
+        );
+      case 4:
+        return (
+          <div className="bg-[#2A2A2A] bg-card rounded-[2.5rem] p-6 h-[50.5vh] shadow-md mr-[18vw]">
+            {children}
+          </div>
+        );
+      default:
+        return "";
+    }
+  };
+
   return (
     <div
       className={cn(
@@ -150,21 +248,38 @@ export default function EnhancedSmoothCarousel({
     >
       <div className="relative overflow-x-auto bg-pink-200 h-[80%] w-full">
         <motion.div className="flex bg-red-500 w-full">
-          <div className="flex-shrink-0 flex items-center w-[36.5vw] h-[51vh] px-4 bg-green-500">
-            this is some contents that would be shown and displayed before the
-            main and major contents here so this should show first,......
+          <div className="flex-shrink-0 flex justify-center items-center w-[36vw] h-[51vh] bg-green-500">
+            <div className="w-[70%] h-full bg-blue-500 flex flex-col justify-between">
+              <div className="w-full h-[10%] bg-red-500">
+              <p className="flex flex-col text-4xl items-start justify-center font-bold">
+                <span className="text-white/85">Amie has all the features.</span>
+                <span className="text-white/60">Both familiar and new.</span>
+              </p>
+              </div>
+
+              <div className="h-[15%] bg-red-500">
+                <p className="flex flex-col text-3xl font-bold items-start justify-start">
+                  <span className="text-white/85">
+                    Available on 
+                  </span>
+                  <span className="text-white/60">
+                  Mac, iOS and Web.
+                  </span>
+                </p>
+              </div>
+            </div>
           </div>
           {cards.map((card, id) => (
             <motion.div
               key={id}
               className={cn("px-4 bg-blue-500 h-full w-full", card.className)}
             >
-              <div className="bg-card rounded-2xl p-6 h-full shadow-md bg-gray-500">
+              <BgReturn cardId={id}>
                 {TopReturn({ cardId: id, cardTitle: card.title })}
                 <div className="text-muted-foreground">
                   {ImageReturn({ cardId: id })}
                 </div>
-              </div>
+              </BgReturn>
             </motion.div>
           ))}
         </motion.div>
