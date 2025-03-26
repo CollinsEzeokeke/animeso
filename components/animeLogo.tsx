6374dc70bbf47700578db4fbf200a5c0615d4c60
Ezeokeke Collins Chidubem <90806850+CollinsEzeokeke@users.noreply.github.com>
2025-03-26 10:49:57 -0700

    ------- 3D target container created and set to meet my needs

---
+++ components/animeLogo.tsx
@@ -0,0 +1,82 @@
+import { Canvas } from "@react-three/fiber";
+import { Model } from "./model";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+
+export default function AmieLogo() {
+  return (
+    <div className="bg-red-500 h-screen w-screen flex justify-center items-center">
+      <Canvas className="bg-green-500 w-[5vw] h-[10vh]">
+        <Scene />
+      </Canvas>
+    </div>
+  );
+}
+
+function Scene() {
+  return (
+    <group>
+      {/* <OrbitControls /> */}
+      <OrbitControls
+        target={[0, 0, 0]} // Orbit around model origin
+        maxPolarAngle={Math.PI / 2} // Limit vertical rotation
+      />
+      {/* <PerspectiveCamera
+        makeDefault
+        fov={45}
+        position={[10, 5, 10]} // Front-right position
+        near={0.1}
+        far={10000}
+      /> */}
+      <gridHelper args={[10, 20]} />
+      <axesHelper args={[5]} />
+      <PerspectiveCamera
+        makeDefault
+        fov={45}
+        position={[5, 10, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+        near={0.1}
+        far={1000}
+      />
+      <directionalLight
+        intensity={10}
+        position={[200, 150, 100]}
+        rotation={[-0.818, 1.364, 0.667]}
+      />
+      <rectAreaLight
+        width={300}
+        height={300}
+        intensity={5}
+        position={[-500, 500, 500]}
+        lookAt={[0, 0, 0]}
+        color="#ffd700"
+      />
+
+      {/* Fill Light (Shadow softening) */}
+      <rectAreaLight
+        width={300}
+        height={300}
+        intensity={2}
+        position={[500, 300, 500]}
+        color="#4090ff"
+      />
+
+      {/* Rim Light (Backlight) */}
+      <directionalLight
+        intensity={15}
+        position={[0, 500, -500]}
+        color="#ffffff"
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+      />
+      <group
+        rotation-x={Math.PI * 0.1}
+        rotation-y={Math.PI * 0.5}
+        rotation-z={Math.PI * 0.5}
+      >
+        <Model />
+      </group>
+      <ambientLight intensity={4} color="#ffffff" />
+    </group>
+  );
+}
---
+++ components/model.tsx
@@ -0,0 +1,60 @@
+import * as THREE from "three";
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+import { JSX } from "react/jsx-runtime";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    ["0309e78c-2629-4355-a846-1ae0fa7398ce"]: THREE.Mesh;
+    ["bba32a21-ebb4-473c-84be-2c3413e60b2e"]: THREE.Mesh;
+    ["693666d9-ead6-441b-a7fc-eeed149ccf1e"]: THREE.Mesh;
+  };
+  materials: {
+    [key: string]: THREE.Material;
+  };
+};
+
+export function Model(props: JSX.IntrinsicElements["group"]) {
+  const gltf = useGLTF("/MyCollection1.glb");
+  const { nodes } = gltf as unknown as GLTFResult;
+  const commonColor = new THREE.MeshStandardMaterial({
+    color: "purple",
+    roughness: 0.3, // More reflective surface
+    metalness: 0.1, // Metallic appearance
+  });
+  return (
+    // formular for calculating pi in code is:
+
+    // number% x 360 / 2 = converted percentage in degrees.............
+    <group
+      {...props}
+      dispose={null}
+      scale={0.002}
+    >
+      <mesh
+        geometry={nodes["0309e78c-2629-4355-a846-1ae0fa7398ce"].geometry}
+        material={commonColor}
+        position={[37.231, 45.131, -273.383]}
+        rotation={[0, 0.104, 0]}
+        scale={2.246}
+      />
+      <mesh
+        geometry={nodes["bba32a21-ebb4-473c-84be-2c3413e60b2e"].geometry}
+        material={commonColor}
+        position={[37.231, 45.131, -273.383]}
+        rotation={[-0.06, 0.104, 0.006]}
+        scale={2.246}
+      />
+      <mesh
+        geometry={nodes["693666d9-ead6-441b-a7fc-eeed149ccf1e"].geometry}
+        material={commonColor}
+        position={[93.487, -109.156, 437.517]}
+        rotation={[-3.045, 0.024, -0.138]}
+        scale={2.246}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/MyCollection1.glb");
